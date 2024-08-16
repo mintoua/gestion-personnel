5faf7e30273a80d4e6ac150327c41112b59d46cb
mintoua <toupkandimintoua@gmail.com>
2024-08-16 19:25:00 +0100

    first-commit

---
+++ src/main/java/com/groupe_5/gestion_personnel/GestionPersonnelApplication.java
@@ -0,0 +1,13 @@
+package com.groupe_5.gestion_personnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPersonnelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPersonnelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupe_5/gestion_personnel/GestionPersonnelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe_5.gestion_personnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPersonnelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
